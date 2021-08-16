34d7e6fe9f80aef92bf4d161a884503370ff2ca8
Marco Eidinger <marco.eidinger@sap.com>
2021-08-16 11:57:31 -0700

    create MSAdaptiveCardExample Xcode project

---
+++ MSAdaptiveCardExample/MSAdaptiveCardExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSAdaptiveCardExampleApp.swift
+//  MSAdaptiveCardExample
+//
+//  Created by Eidinger, Marco on 8/16/21.
+//
+
+import SwiftUI
+
+@main
+struct MSAdaptiveCardExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
