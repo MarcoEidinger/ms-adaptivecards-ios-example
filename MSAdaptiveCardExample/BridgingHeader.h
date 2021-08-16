37225acd9145bda619cac892b8804de427cd6a8b
Marco Eidinger <marco.eidinger@sap.com>
2021-08-16 12:28:54 -0700

    create bridging header to access AdaptiveCards/ACFramework

---
+++ MSAdaptiveCardExample/BridgingHeader.h
@@ -0,0 +1,13 @@
+//
+//  BridgingHeader.h
+//  MSAdaptiveCardExample
+//
+//  Created by Eidinger, Marco on 8/16/21.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import "AdaptiveCards/ACFramework.h"
+
+#endif /* BridgingHeader_h */
